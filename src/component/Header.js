a3f26df1361386156ca5bb240459875e3b4e145c
Ekkachai <eakton22@gmail.com>
2020-08-22 01:38:35 +0700

    add 5555

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <header className="w-full top-0 bg-img">
             <div className="container flex flex-col mx-auto py-6 px-6">
                 <div className="flex-col font-GG text-gray-100">
-                    <a className="text-xl sm:text-4xl">Situation Reports</a>
+                    <a className="text-xl sm:text-4xl">Situation Reports5555555555</a>
                     <a className="text-sm">&nbsp;By Ekkachai</a>
                 </div>
             </div>
