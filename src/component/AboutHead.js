f82e06577ad3cd0537c20370e338b710916f86d0
Ekkachai <eakton22@gmail.com>
2020-08-23 06:08:44 +0700

    call API success

--- src/component/AboutHead.js
+++ src/component/AboutHead.js
@@ -3,7 +3,7 @@ import '../asset/codestyle.css'
 
 function AboutHead() {
     return(
-        <div className="flex justify-center sm:flex-col px-6 pt-6 sm:pt-20 sm:px-32">
+        <div className="flex justify-center sm:flex-col px-6 pt-8 sm:pt-20 sm:px-32">
             <p className="font-gg text-gray-400 text-2xl sm:text-3xl">
                 Coronavirus
             </p>
--- src/component/ReportC.js
+++ src/component/ReportC.js
@@ -3,9 +3,48 @@ import '../asset/codestyle.css'
 
 function ReportC() {
     return(
-        <div>
-
-        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>111</td>
+                <td>222</td>
+                <td>333</td>
+                <td>444</td>
+                <td>555</td>
+                </tr>
+                <tr>
+                {/* <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td> */}
+                </tr>
+            </tbody>
+            </table>
     )
 }
 
