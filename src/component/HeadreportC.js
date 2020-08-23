07ae5c2d4787df6e36eab7a2562553778a0b9548
Ekkachai <eakton22@gmail.com>
2020-08-23 20:44:11 +0700

    Add animation

---
+++ src/component/HeadreportC.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../asset/codestyle.css'
+
+function HeadreportC() {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default HeadreportC
