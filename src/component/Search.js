6c218078dfb8d84a3e8cdb6ce3085ab7ab3e1a8a
Ekkachai <eakton22@gmail.com>
2020-08-24 05:37:21 +0700

    search

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../asset/codestyle.css'
 
-function Search() {
-    return(
-        <div className="py-4">
-            <input className="w-full border-solid text-xl px-4 py-4" placeholder="Search"/>
-        </div>
-    )
+export class Search extends Component {
+    // searchname = (keyword) => {
+    //     console.log(keyword)
+    //     const url = "https://api.covid19api.com/summary?fbclid=IwAR0uoH6cU95rR1X8eiYo7iUwWz0bVGViIjc4Hx8J8o36idD48kD_VflcOyg";
+    //     const response = fetch(url);
+    //     const data = response.json();
+    //     console.log(data.Countries);
+    // }
+    state = {
+        loading: true,
+        list: [],
+    };
+    async searchname(keyword) {
+        const url = "https://api.covid19api.com/summary?fbclid=IwAR0uoH6cU95rR1X8eiYo7iUwWz0bVGViIjc4Hx8J8o36idD48kD_VflcOyg";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({list:data.Countries, loading: false});
+    }
+    render() {
+        return (
+            <div className="py-4">
+                <input className="w-full border-solid text-xl px-4 py-4" placeholder="Search" onChange={(event) => {this.searchname(event.target.value)}}/>
+            </div>
+        )
+    }
 }
 
 export default Search
