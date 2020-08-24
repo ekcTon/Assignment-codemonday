b1701780be447f63aa180831011f36ad00290a99
Ekkachai <eakton22@gmail.com>
2020-08-24 18:21:29 +0700

    search

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react'
 import '../asset/codestyle.css'
+import Axios from 'axios'
 
 export class Search extends Component {
-    // searchname = (keyword) => {
-    //     console.log(keyword)
-    //     const url = "https://api.covid19api.com/summary?fbclid=IwAR0uoH6cU95rR1X8eiYo7iUwWz0bVGViIjc4Hx8J8o36idD48kD_VflcOyg";
-    //     const response = fetch(url);
-    //     const data = response.json();
-    //     console.log(data.Countries);
-    // }
-    state = {
-        loading: true,
-        list: [],
-    };
-    async searchname(keyword) {
-        const url = "https://api.covid19api.com/summary?fbclid=IwAR0uoH6cU95rR1X8eiYo7iUwWz0bVGViIjc4Hx8J8o36idD48kD_VflcOyg";
-        const response = await fetch(url);
-        const data = await response.json();
-        this.setState({list:data.Countries, loading: false});
+    constructor(props){
+        super(props)
+        this.state = {rows: [] }
+    }
+    searchname = (keyword) => {
+        console.log(keyword)
+        var dataArray = []
+        Axios.get("https://api.covid19api.com/summary?fbclid=IwAR0uoH6cU95rR1X8eiYo7iUwWz0bVGViIjc4Hx8J8o36idD48kD_VflcOyg" + keyword).then(result=>{
+            // console.log(JSON.stringify(result.data.Countries))
+            result.data.Countries.forEach(item => {
+                dataArray.push(item)
+            })
+            this.setState({rows:dataArray})
+        })
     }
     render() {
         return (
             <div className="py-4">
                 <input className="w-full border-solid text-xl px-4 py-4" placeholder="Search" onChange={(event) => {this.searchname(event.target.value)}}/>
+                {this.state.rows.map(item=> (
+                    <div>
+                        <strong>{item.CountryCode}</strong>
+                    </div>
+                ))}
+                
             </div>
         )
     }
